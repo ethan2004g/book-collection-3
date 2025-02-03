fca40005fdcb904bfb2dab05fdfb0ae84a5091cd
ethan2004g <ethan2004g@tamu.edu>
2025-02-03 21:33:30 +0000

    finished book collction 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :user_books
   resources :users
   resources :books
-  root 'books#index'
+  #root 'books#index'
+  root 'user_books#index'
+
   get '/books', to: redirect('/') 
   get '/addbook', to: 'books#new'
   post '/addbook', to: 'books#create'
@@ -12,5 +14,11 @@ Rails.application.routes.draw do
   delete '/remove/:title', to: 'books#destroy', as: 'remove_book'
   get '/remove/:title', to: 'books#destroy'
   get '/remove/:title/confirm', to: 'books#delete', as: 'confirm_remove_book'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
 
 end
